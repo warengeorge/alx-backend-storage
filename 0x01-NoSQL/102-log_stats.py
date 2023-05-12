6a39b2883dbbf8d89e6b3b98a6bf59a18ca52044
warengeorge <georgeomiebi@gmail.com>
2023-05-12 14:42:58 +0100

    Added 102-log_status.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Provides more stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+def nginx_stats_check():
+    """ provides some stats about Nginx logs stored in MongoDB:"""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    num_of_docs = collection.count_documents({})
+    print("{} logs".format(num_of_docs))
+    print("Methods:")
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for md in methods_list:
+        method_count = collection.count_documents({"method": md})
+        print("\tmethod {}: {}".format(md, method_count))
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
+
+    print("IPs:")
+
+    tp_ips = collection.aggregate([
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+    for tp in tp_ips:
+        count = tp.get("count")
+        ip_address = tp.get("ip")
+        print("\t{}: {}".format(ip_address, count))
+
+
+if __name__ == "__main__":
+    nginx_stats_check()
